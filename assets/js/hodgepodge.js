ea8c19036b4bf8c4997729d14b95b48d50a739fe
Monica K <postcardsfromraingirl@gmail.com>
2024-11-28 14:08:10 -0800

    first attempt at new commit

--- assets/js/hodgepodge.js
+++ assets/js/hodgepodge.js
@@ -4,7 +4,7 @@ words = [
 "Time to zoom",
 "[Absence, Absinthe, Abstinence] makes the heart grow fonder.",
 "6/12/2019: Next Level Charli - Charli XCX",
-"Birthday wishlist: mbira, a happy sprawling chili plant, ReMarkable tablet. I actually received and made/bought/received everything I wanted in late 2019/early 2020 :D",
+"Birthday wishlist: mbira, a happy sprawling chili plant. I actually received and made/bought/received everything I wanted in late 2019/early 2020. Right before COVID.",
 "Apparently 'This Song is Not For Anyone But You' became 'Twilight' in English....<a href='https://www.youtube.com/watch?v=gd38-X3HpbM' target='_blank'>https://www.youtube.com/watch?v=gd38-X3HpbM</a>",
 "\"meeting the wrong person at the right time\"",
 "Would You Rather: Eat a whole stuffed camel or a vegducken?",
@@ -13,8 +13,8 @@ words = [
 //img link, html to inject
 me = [
 ["../assets/images/about/baby.jpg", "<p><b>Likes:</b> Lion King, ants, apple juice, game where fit shapes into holes</p><p><b>Dislikes:</b> Toothbrush</p>"],
-["../assets/images/about/17.jpg", "<a href='https://earthzine.org/fire-and-ice-wars-battles-in-the-sierra-nevada-mountains/' target='_blank'>Environmental science</a> and loads of <a href='https://www.github.com/fantastic-books-in-clips' target='_blank'>books.</a>"],
-["../assets/images/about/23.jpg", "Working in a foreign country and wandering into a showing of <a href='https://www.youtube.com/watch?v=WQ_Yo06kIIA' target='_blank'>Hausu</a>. I want to paint in Japan and make a kalimba."],
+["../assets/images/about/17.jpg", "<a href='https://earthzine.org/fire-and-ice-wars-battles-in-the-sierra-nevada-mountains/' target='_blank'>Environmental science</a> and quite a few thoughts about<a href='https://github.com/persinammon4/fantastic-books-in-clips' target='_blank'>books.</a>"],
+["../assets/images/about/23.jpg", "Working in a foreign country and wandering into a showing of <a href='https://www.youtube.com/watch?v=WQ_Yo06kIIA' target='_blank'>Hausu</a>."],
 ["../assets/images/about/rooster.jpg", "It's a rooster."],
 ["../assets/images/about/moon_ttyl.jpeg", "Sonic Interlude: Romanticism; transcendence;;(Bram Stoker Bram's Strokes | Mary & Percy are two sides of the same coin; {a sophomoric victory in finding the meaning of life | the pinnacle of the West }; <br /> communion with nature is the only salvation there is); <br /> nevermore nevermore; I dedicate this soliloquy to e.e. cumm(in)gs x h[e]art crane's very own love child, which is to say nobody in particular at all. <br /> The leeward side of the moon // its friendly face to us and them."]
 ];
